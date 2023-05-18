d05b37d4a8642538f60cc4db3c345d7fbb869d70
ajayhoke <133940540+ajayhoke@users.noreply.github.com>
2023-05-18 19:22:46 +0530

    addition programm

---
+++ Ass3.java
@@ -0,0 +1,20 @@
+public class Ass3
+{
+	public static void main(String[]a)
+	{
+		String ans="mul";
+		int no1=3,no2=5;
+		switch(ans)
+		{
+		case "add":
+			System.out.println("Addition  :"+(no1+no2));
+			break;
+		case "sub":
+			System.out.println("Subtraction  :"+(no1-no2));
+			break;
+		case "mul":
+			System.out.println("Multiplication :"+(no1*no2));
+			break;
+		}
+	}
+}
